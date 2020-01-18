ca46e156e1ac4836d2529f66420ba40551209f0b
Nadeesha Maduman <>
2020-01-18 23:30:05 +0530

    initial commit

---
+++ android/app/src/main/kotlin/blocsample/nma/com/flutter_bloc_pattern_sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package blocsample.nma.com.flutter_bloc_pattern_sample
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_pattern_sample/src/app.dart';
+
+void main() => runApp(App());
